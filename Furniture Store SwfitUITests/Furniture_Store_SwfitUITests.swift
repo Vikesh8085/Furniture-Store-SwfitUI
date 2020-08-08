d91bdadfe293abd6d54282487000d068f4d018c9
vikesh <vikesh.prasad@mobiotics.com>
2020-08-08 18:09:39 +0530

    Initial Commit

---
+++ Furniture Store SwfitUITests/Furniture_Store_SwfitUITests.swift
@@ -0,0 +1,34 @@
+//
+//  Furniture_Store_SwfitUITests.swift
+//  Furniture Store SwfitUITests
+//
+//  Created by Vikesh Prasad on 08/08/20.
+//  Copyright © 2020 vikeshapps. All rights reserved.
+//
+
+import XCTest
+@testable import Furniture_Store_SwfitUI
+
+class Furniture_Store_SwfitUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
